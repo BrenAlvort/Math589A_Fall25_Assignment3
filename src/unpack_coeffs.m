3bdacefe7e3253f5ef41d60a65c9d72723bc8924
BrenAlvort <bkao21@ciencias.unam.mx>
2025-11-08 16:21:01 -0700

    Update unpack_coeffs.m

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,6 +1,20 @@
 function coef = unpack_coeffs(beta, N, K)
-    coef.c = beta(1);
-    coef.a = beta(2:1+N);
-    coef.alpha = beta(1+N+1 : 1+N+K);
-    coef.beta  = beta(1+N+K+1 : 1+N+2*K);
+% UNPACK_COEFFS  helper matching our indexing convention
+beta = beta(:);
+coef.c = beta(1);
+coef.d = beta(2);
+coef.a = zeros(max(0,N),1);
+coef.alpha = zeros(max(0,K),1);
+coef.beta = zeros(max(0,K),1);
+
+idx = 3;
+for i = 1:N
+    coef.a(i) = beta(idx); idx = idx + 1;
+end
+for k = 1:K
+    coef.alpha(k) = beta(idx); idx = idx + 1;
+end
+for k = 1:K
+    coef.beta(k) = beta(idx); idx = idx + 1;
+end
 end
