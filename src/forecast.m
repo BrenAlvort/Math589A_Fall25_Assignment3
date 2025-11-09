e6ddf66dd1704b7fdd345eec1a1e8855b15e562f
Nishanta Baral <nishantabaral48@gmail.com>
2025-11-09 00:26:32 -0700

    fixed bugs

--- src/forecast.m
+++ src/forecast.m
@@ -2,18 +2,18 @@ function yF = forecast(y, s, coef, H)
 y = y(:); T = numel(y); N = numel(coef.a); K = numel(coef.alpha);
 yF = zeros(H,1);
 for h = 1:H
-    t = T + h;
+    t = T + h;                                  % absolute time
     sea = 0;
     for k = 1:K
         sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
     end
-    acc = coef.c + coef.d*t + sea;                 % adding trend term
+    acc = coef.c + coef.d*t + sea;              % include trend with absolute t
     for i = 1:N
-        idx = t - i;                               % y_{t-i}
+        idx = t - i;
         if idx <= T
-            acc = acc + coef.a(i) * y(idx);        
+            acc = acc + coef.a(i) * y(idx);
         else
-            acc = acc + coef.a(i) * yF(idx - T);  
+            acc = acc + coef.a(i) * yF(idx - T);
         end
     end
     yF(h) = acc;
