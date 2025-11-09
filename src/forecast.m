5d083d03bfa9887142de8112513f1a65194850d5
Nishanta Baral <nishantabaral48@gmail.com>
2025-11-09 00:44:53 -0700

    fixed bugs

--- src/forecast.m
+++ src/forecast.m
@@ -1,13 +1,15 @@
 function yF = forecast(y, s, coef, H)
 y = y(:); T = numel(y); N = numel(coef.a); K = numel(coef.alpha);
+M = T - N;
 yF = zeros(H,1);
 for h = 1:H
-    t = T + h;                                  % absolute time
+    t = T + h;                 % absolute time for season/lag
+    r = M + h;                 % row-index for trend
     sea = 0;
     for k = 1:K
         sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
     end
-    acc = coef.c + coef.d*t + sea;              % include trend with absolute t
+    acc = coef.c + coef.d*r + sea;     % trend uses r
     for i = 1:N
         idx = t - i;
         if idx <= T
