3a8306791d8345bb66f3e96a3073a1548739890b
BrenAlvort <bkao21@ciencias.unam.mx>
2025-11-09 12:21:24 -0700

    Update score_model.m

--- src/score_model.m
+++ src/score_model.m
@@ -1,8 +1,13 @@
 function S = score_model(RSS, M, p, criterion)
     if nargin<4 || isempty(criterion), criterion = 'bic'; end
-    if strcmpi(criterion,'bic')
-        S = M*log(RSS/M) + p*log(M);
-    else
-        S = M*log(RSS/M) + 2*p;
+    switch lower(criterion)
+        case 'bic'
+            if RSS <= 0, RSS = max(RSS, eps); end
+            S = M*log(RSS/M) + p*log(M);
+        case 'aic'
+            if RSS <= 0, RSS = max(RSS, eps); end
+            S = M*log(RSS/M) + 2*p;
+        otherwise
+            error('Unknown criterion');
     end
 end
